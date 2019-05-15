ebf98e8a3b924abc1702cae750a5e500b63ca0c7
Andreas Skorczyk <me@andreas-sk.de>
2019-05-15 18:41:11 +0200

    Autostart pulsar & uncomment galaxy-dependencies

--- ansible/files/pulsar/local_env.sh
+++ ansible/files/pulsar/local_env.sh
@@ -6,14 +6,14 @@ export DRMAA_LIBRARY_PATH=/usr/lib64/condor/libdrmaa.so
 
 ## If you wish to use a variety of Galaxy tools that depend on galaxy.eggs being defined,
 ## set GALAXY_HOME to point to a copy of Galaxy.
-export GALAXY_HOME=/opt/pulsar/dependencies/galaxy
-
-# https://github.com/galaxyproject/pulsar/issues/134
-export PYTHONPATH=${PYTHONPATH}:/opt/pulsar/dependencies/galaxy/lib
-export GALAXY_LIB=/opt/pulsar/dependencies/galaxy/lib
-
-## Uncomment to verify GALAXY_HOME is set properly before starting the LWR.
-export TEST_GALAXY_LIBS=1
+# export GALAXY_HOME=/opt/pulsar/dependencies/galaxy
+#
+# # https://github.com/galaxyproject/pulsar/issues/134
+# export PYTHONPATH=${PYTHONPATH}:/opt/pulsar/dependencies/galaxy/lib
+# export GALAXY_LIB=/opt/pulsar/dependencies/galaxy/lib
+#
+# ## Uncomment to verify GALAXY_HOME is set properly before starting the LWR.
+# export TEST_GALAXY_LIBS=1
 
 ## If using a manager that runs jobs as real users, be sure to load your Python
 ## environement in here as well.
